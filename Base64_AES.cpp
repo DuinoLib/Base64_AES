b4cc70e55f1f143cb51a879fa03370e39be63a56
GluTbl <30377623+GluTbl@users.noreply.github.com>
2020-03-21 18:26:37 +0530

    Update Base64_AES.cpp

--- Base64_AES.cpp
+++ Base64_AES.cpp
@@ -83,7 +83,7 @@ void Base64_AES::decrypt_b64(char * encrypted, int msg_length, char * decrypted_
      convert the base64 encoded message to byte array
   */
   int expected_decode_length = base64_decode_expected_len(msg_length);
-  if(msg_length<expected_decode_length){
+  if(expected_decode_length<32){
   	return;
   }
   char *decoded = new char[expected_decode_length];
